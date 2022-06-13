52d8b91110544e8743368fb8106c1646276d5a23
tygoto <tygoto@me.com>
2022-06-13 20:37:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# ros2web_turtlesim
+
+
+ROS2Web must be installed before you attempt to install ros2web_turtlesim.
+
+### ROS2Web
+https://github.com/cpc-lab-robotics/ros2web
+
+
+# Install
+```zsh
+cd ros2_ws/src
+git clone https://github.com/cpc-lab-robotics/ros2web_turtlesim
+colcon build --symlink-install
+. ./install/local_setup.zsh
+```
+
+# Usage
+
+```zsh
+ros2 web server
+```
+
+After starting up the server, try accessing this URL.
+
+http://localhost:8080/ui/turtlesim
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+package_name = 'ros2web_turtlesim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'ros2web'],
+    zip_safe=True,
+    maintainer='tygoto',
+    maintainer_email='tygoto@me.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'ros2web.package': [
+            'turtlesim = ros2web_turtlesim:ROS2WebTurtlesim'
+        ],
+    },
+    package_data={
+        'ros2web_turtlesim': [
+            'data/**/*',
+        ],
+    },
+)
